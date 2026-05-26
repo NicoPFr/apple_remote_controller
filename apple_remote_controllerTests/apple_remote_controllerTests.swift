97a52d4e3af7301b15315930deb291cbb3983258
Nicolas <nicolas.peeters@orange.com>
2026-05-26 10:00:30 +0200

    Initial Commit

---
+++ apple_remote_controllerTests/apple_remote_controllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  apple_remote_controllerTests.swift
+//  apple_remote_controllerTests
+//
+//  Created by Nicolas Peeters on 26/05/2026.
+//
+
+import Testing
+@testable import apple_remote_controller
+
+struct apple_remote_controllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
